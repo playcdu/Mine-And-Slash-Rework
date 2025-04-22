17cd4940ab438138c2865bdfe1669a113a20cc4e
TUsama <1716976420@qq.com>
2025-04-22 11:48:56 +0800

    add config

--- src/main/java/com/robertx22/mine_and_slash/capability/player/container/BackpackQuickLootButton.java
+++ src/main/java/com/robertx22/mine_and_slash/capability/player/container/BackpackQuickLootButton.java
@@ -1,6 +1,7 @@
 package com.robertx22.mine_and_slash.capability.player.container;
 
 import com.robertx22.library_of_exile.main.Packets;
+import com.robertx22.mine_and_slash.config.forge.ClientConfigs;
 import com.robertx22.mine_and_slash.mmorpg.SlashRef;
 import com.robertx22.mine_and_slash.uncommon.localization.Gui;
 import com.robertx22.mine_and_slash.vanilla_mc.packets.backpack.BackPackLootMenuPacket;
@@ -17,14 +18,14 @@ import net.minecraftforge.client.event.ScreenEvent;
 public class BackpackQuickLootButton extends ImageButton {
     static ResourceLocation TEXTURE = SlashRef.id("textures/gui/backpack_button.png");
     public BackpackQuickLootButton(int pX, int pY, ContainerScreen screen) {
-        super(pX, pY, 14, 14, 0, 0, 16, TEXTURE, 16, 32, x -> Packets.sendToServer(new BackPackLootMenuPacket()), Gui.MASTER_BACKPACK_LOOT_BUTTON.locName());
+        super(pX + ClientConfigs.getConfig().QUICK_LOOT_BUTTON_X_OFFSET.get(), pY + ClientConfigs.getConfig().QUICK_LOOT_BUTTON_Y_OFFSET.get(), 14, 14, 0, 0, 16, TEXTURE, 16, 32, x -> Packets.sendToServer(new BackPackLootMenuPacket()), Gui.MASTER_BACKPACK_LOOT_BUTTON.locName());
         this.setTooltip(Tooltip.create(Gui.MASTER_BACKPACK_LOOT_BUTTON.locName()));
     }
     
 
     public static void addLootButton(ScreenEvent.Init event){
         if (!(event instanceof ScreenEvent.Init.Post)) return;
-        if (Minecraft.getInstance().screen instanceof ContainerScreen containerScreen){
+        if (Minecraft.getInstance().screen instanceof ContainerScreen containerScreen && ClientConfigs.getConfig().ENABLE_QUICK_LOOT_BUTTON.get()){
             event.addListener(new BackpackQuickLootButton(containerScreen.getGuiLeft() + containerScreen.getXSize(), containerScreen.getGuiTop(), containerScreen));
         }
     }
